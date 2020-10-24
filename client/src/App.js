67d020bdf55d473f7631ba3c8465257cc5b90ab6
doge33 <sandylx33@gmail.com>
2020-10-24 01:26:36 +0000

    removed redundant useEffect(can be found in useApplicationData

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import useApplicationData from './hooks/useApplicationData';
 import './App.css';
-import { SET_USERS } from './reducers/dataReducer';
-
-
-
 
 function App() {
 
   const { state, dispatch } = useApplicationData();
-
-  // useEffect(() => {
-
-  //   axios({
-  //     method: 'GET',
-  //     url: '/api/users'
-  //   })
-  //     .then(result => {
-  //       dispatch({ type: SET_USERS, users: result.data })
-  //       console.log("inside axios request in App.js")
-  //     })
-  //     .catch(err => console.log(err.message))
-
-  // }, [])
 
   const userList = state.users.map((user) => (<li key={user.id}> {user.first_name} {user.last_name} {user.email}</li>));
 
