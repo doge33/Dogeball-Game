8c165f0905543ac7179a70347319965b08d74092
Dani <dreamboy.dani@gmail.com>
2020-10-25 21:02:30 -0600

    Add the password authentication and installed BCrypt

--- rails_backend/app/controllers/api/users_controller.rb
+++ rails_backend/app/controllers/api/users_controller.rb
@@ -17,6 +17,6 @@ class Api::UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password_digest)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
