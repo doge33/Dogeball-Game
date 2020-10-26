5755cf41d3b8716fa4ba84e57adb91216fbd9f18
Dani <dreamboy.dani@gmail.com>
2020-10-25 20:01:23 -0600

    Add the logic to setState of the controlled input

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -3,34 +3,71 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 //import className from "classnames";
 
-function Signup (props) {
-  return(
+function Signup(props) {
+
+  const [state, setState] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirm_password: ''
+  })
+
+  const handleChange = event => {
+    const value = event.target.value
+    setState({
+      ...state,
+      [event.target.name]: value
+    });
+  }
+  return (
     <main>
       <section>
-      <Form>
-      <Form.Group controlId="formBasicText">
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Enter username" />
-        </Form.Group>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
+        <Form onSubmit={event => event.preventDefault()}>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
+          <Form.Group controlId="formBasicText">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              name="username"
+              type="text"
+              placeholder="Enter username"
+              value={state.username}
+              onChange={handleChange} />
+          </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control type="password" placeholder="Confirm Password" />
-        </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              name="email"
+              type="email"
+              placeholder="Enter email"
+              value={state.email}
+              onChange={handleChange} />
+          </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Signup
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={state.password}
+              onChange={handleChange} />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              name="confirm_password"
+              type="password"
+              placeholder="Confirm Password"
+              value={state.confirm_password}
+              onChange={handleChange} />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Signup
         </Button>
-      </Form>
+        </Form>
       </section>
     </main>
 
