43eebe3e860844a3b02a07f3687493b18a8eac50
doge33 <sandylx33@gmail.com>
2020-10-27 15:23:00 +0000

    trying to stash

--- client/src/routes/Game.js
+++ client/src/routes/Game.js
@@ -4,16 +4,9 @@ import className from "classnames";
 import Newcamera from "../components/Game/Newcamera"
 
 function Game() {
-<<<<<<< HEAD
-  //if mode === PREGAME
-  return (
-    <div>
-      <Camera width={"500"} height={"500"} />
-=======
     //if mode === PREGAME
   return(  <div>
     <Newcamera />
->>>>>>> get-coordinates
     </div>
   )
 }
