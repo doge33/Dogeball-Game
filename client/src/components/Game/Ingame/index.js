2d60f090e9dad371cc4a5de906131208168e29f9
doge33 <sandylx33@gmail.com>
2020-10-30 03:03:12 +0000

    added unfinished timer; drew lay-out for game

--- client/src/components/Game/Ingame/index.js
+++ client/src/components/Game/Ingame/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Button from "../../Button";
+import Timer from "./Timer";
+
+import "./Ingame.scss";
 
 function Ingame(props) {
 
   return (
-    <div>
-      <h1>Hello this is In-game mode</h1>
-      <Button onClick={props.onClick}>Game finished. Go back to game lobby mode</Button>
+    <div className="in-game">
+      <div className="score"><h1>This may be the score component</h1></div>
+      <div className="timer"><Timer /></div>
+      <div className="game-area">
+        <h1>This may be the game area component</h1>
+        <Button onClick={props.onClick}>Quit button: Go back to game lobby mode</Button>
+        <div className="countdowns">
+          <h2>Another component that renders conditionally on different modes:
+            countdown to start; game-over(result+countdown to quit); </h2>
+          <Button>Restart button(click before countdown finishes)</Button>
+        </div>
+        
+      </div>
     </div>
 
   )
