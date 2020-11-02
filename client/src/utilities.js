bd9624f8919a4c874333c1b5b639d60463ad1827
Michael Klepac <mklepac90@gmail.com>
2020-11-02 00:33:43 +0000

    rendering images on canvas

--- client/src/utilities.js
+++ client/src/utilities.js
@@ -16,7 +16,8 @@
  */
 import * as posenet from '@tensorflow-models/posenet';
 import * as tf from '@tensorflow/tfjs';
-import img from './components/Game/hand.png';
+import img from './components/Game/doge_ball.png';
+import img2 from './components/Game/nyan_cat.png';
 
 // =======================================================
 // Global constants for utilities
@@ -85,24 +86,23 @@ export function drawKeypointsAvatar(ctx, keypoints, minConfidence, scale = 1) {
 // ----------------------------------------------------
 // * Draw specified image on canvas
 // ----------------------------------------------------
-export function renderImageToCanvas2(ctx, x, y) {
-  // const ctx = canvas.current.getContext('2d');
-  // canvas.current.width = window.innerWidth;
-  // canvas.current.height = window.innerHeight;
+export function renderImageToCanvas2(ctx, x, y, bad = false) {
 
-  const imgTag = new Image();
-  imgTag.src = img;   // load image
+  const projectile = new Image();
+  const badProjectile = new Image();
+  projectile.src = img;   // load image
+  badProjectile.src = img2;
 
-  ctx.drawImage(imgTag, x, y, 100, 120);
+  if (!bad) {
+    ctx.drawImage(projectile, x, y, 120, 140);
+  } else {
+    ctx.drawImage(badProjectile, x, y, 75, 75);
+  }
 }
 // ----------------------------------------------------
 // * Draw projectile on canvas
 // ----------------------------------------------------
-export function generateProjectile(ctx, videoWidth, videoHeight, projectileCoords, r, color) {   
-
-  const hitboxWidth = r * 2.5;
-  const hitboxHeight = r * 2.5;
-  const badColor = "red";
+export function generateProjectile(ctx, videoWidth, videoHeight, projectileCoords) {   
 
   projectileCoords.forEach((pair, index) => {
     if (index !== 0) { // exclude item at index 0 from being rendered
@@ -118,16 +118,14 @@ export function generateProjectile(ctx, videoWidth, videoHeight, projectileCoord
       
       // Render projectile || pair[2] % 4 === 0
       if (index === 2 || index === 6) {
-        drawPoint(ctx, y, x, r, badColor);
+        renderImageToCanvas2(ctx, x, y, true);
+        const rect = {x: x, y: y, width: 75, height: 75};
+        drawBoundingBox2(ctx, rect);
       } else {
-        drawPoint(ctx, y, x, r, color);
+        renderImageToCanvas2(ctx, x, y);
+        const rect = {x: x + 25, y: y + 35, width: 75, height: 75};
+        drawBoundingBox2(ctx, rect);
       }
-      
-      // Calculate dimensions of hitbox
-      const rect = {x: x - (hitboxWidth / 2), y: y - (hitboxHeight / 2), width: hitboxWidth, height: hitboxHeight};
-      
-      // Render hitbox
-      drawBoundingBox2(ctx, rect);
     }
   }); 
 }
@@ -164,12 +162,10 @@ export function detectCollision(avatar, projectiles) {
 // -----------------------------------------------------------------
 // * Calculates hitboxes and runs them through collision detector
 // -----------------------------------------------------------------
-export function collisionDetection(pose, minConfidence, projectileCoords, videoWidth, videoHeight, r) {
+export function collisionDetection(pose, minConfidence, projectileCoords, videoWidth, videoHeight) {
   let keypoints = pose["keypoints"];
   let poseHitboxes = [];
   let projectileHitboxes = [];
-  const hitboxWidth = r * 2.5;
-  const hitboxHeight = r * 2.5;
 
   // Avatar Hitboxes
   for (let i = 0; i < keypoints.length; i++) {
@@ -195,8 +191,8 @@ export function collisionDetection(pose, minConfidence, projectileCoords, videoW
   // Projectile Hitboxes
   projectileCoords.forEach((pair, index) => {
     if (index !== 0) { // exclude item at index 0 from hitbox calculations
-      let x = pair[0] * (videoWidth);
-      let y = pair[1] * (videoHeight);
+      let x = pair[0] * videoWidth;
+      let y = pair[1] * videoHeight;
   
       // Constrain spawn area of projectile 
       if (x < 100) {
@@ -204,10 +200,15 @@ export function collisionDetection(pose, minConfidence, projectileCoords, videoW
       } else if (x > (videoWidth * .90)) {
         x -= (videoWidth * .1);
       }
-  
+
       // Calculate dimensions of hitbox
-      const rect = {x: x - (hitboxWidth / 2), y: y - (hitboxHeight / 2), width: hitboxWidth, height: hitboxHeight, projectileIndex: index, isBad: pair[2]};
-      projectileHitboxes.push(rect);
+      if (index === 2 || index === 6) {
+        const rect = {x: x, y: y, width: 75, height: 75, projectileIndex: index, isBad: pair[2]};
+        projectileHitboxes.push(rect);
+      } else {
+        const rect = {x: x + 25, y: y + 35, width: 75, height: 75, projectileIndex: index, isBad: pair[2]};
+        projectileHitboxes.push(rect);
+      }
     }
   });
 
@@ -283,173 +284,173 @@ export function shiftCoordinates(array) {
 /**
  * Draws a line on a canvas, i.e. a joint
  */
-export function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
-  ctx.beginPath();
-  ctx.moveTo(ax * scale, ay * scale);
-  ctx.lineTo(bx * scale, by * scale);
-  ctx.lineWidth = lineWidth;
-  ctx.strokeStyle = color;
-  ctx.stroke();
-}
+// export function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
+//   ctx.beginPath();
+//   ctx.moveTo(ax * scale, ay * scale);
+//   ctx.lineTo(bx * scale, by * scale);
+//   ctx.lineWidth = lineWidth;
+//   ctx.strokeStyle = color;
+//   ctx.stroke();
+// }
 
-/**
- * Draws a pose skeleton by looking up all adjacent keypoints/joints
- */
-export function drawSkeleton(keypoints, minConfidence, ctx, scale = 1) {
-  const adjacentKeyPoints =
-      posenet.getAdjacentKeyPoints(keypoints, minConfidence);
+// /**
+//  * Draws a pose skeleton by looking up all adjacent keypoints/joints
+//  */
+// export function drawSkeleton(keypoints, minConfidence, ctx, scale = 1) {
+//   const adjacentKeyPoints =
+//       posenet.getAdjacentKeyPoints(keypoints, minConfidence);
 
-  adjacentKeyPoints.forEach((keypoints) => {
-    drawSegment(
-        toTuple(keypoints[0].position), toTuple(keypoints[1].position), color,
-        scale, ctx);
-  });
-}
+//   adjacentKeyPoints.forEach((keypoints) => {
+//     drawSegment(
+//         toTuple(keypoints[0].position), toTuple(keypoints[1].position), color,
+//         scale, ctx);
+//   });
+// }
 
-/**
- * Draw pose keypoints onto a canvas
- */
-export function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
-  for (let i = 0; i < keypoints.length; i++) {
-    const keypoint = keypoints[i];
+// /**
+//  * Draw pose keypoints onto a canvas
+//  */
+// export function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
+//   for (let i = 0; i < keypoints.length; i++) {
+//     const keypoint = keypoints[i];
 
-    if (keypoint.score < minConfidence) {
-      continue;
-    }
+//     if (keypoint.score < minConfidence) {
+//       continue;
+//     }
 
-    const {y, x} = keypoint.position;
-    drawPoint(ctx, y * scale, x * scale, 3, color);
-  }
-}
+//     const {y, x} = keypoint.position;
+//     drawPoint(ctx, y * scale, x * scale, 3, color);
+//   }
+// }
 
-/**
- * Draw the bounding box of a pose. For example, for a whole person standing
- * in an image, the bounding box will begin at the nose and extend to one of
- * ankles
- */
-export function drawBoundingBox(keypoints, ctx) {
-  const boundingBox = posenet.getBoundingBox(keypoints);
+// /**
+//  * Draw the bounding box of a pose. For example, for a whole person standing
+//  * in an image, the bounding box will begin at the nose and extend to one of
+//  * ankles
+//  */
+// export function drawBoundingBox(keypoints, ctx) {
+//   const boundingBox = posenet.getBoundingBox(keypoints);
 
-  ctx.rect(
-      boundingBox.minX, boundingBox.minY, boundingBox.maxX - boundingBox.minX,
-      boundingBox.maxY - boundingBox.minY);
+//   ctx.rect(
+//       boundingBox.minX, boundingBox.minY, boundingBox.maxX - boundingBox.minX,
+//       boundingBox.maxY - boundingBox.minY);
 
-  ctx.strokeStyle = boundingBoxColor;
-  ctx.stroke();
-}
+//   ctx.strokeStyle = boundingBoxColor;
+//   ctx.stroke();
+// }
 
-/**
- * Converts an arary of pixel data into an ImageData object
- */
-export async function renderToCanvas(a, ctx) {
-  const [height, width] = a.shape;
-  const imageData = new ImageData(width, height);
+// /**
+//  * Converts an arary of pixel data into an ImageData object
+//  */
+// export async function renderToCanvas(a, ctx) {
+//   const [height, width] = a.shape;
+//   const imageData = new ImageData(width, height);
 
-  const data = await a.data();
+//   const data = await a.data();
 
-  for (let i = 0; i < height * width; ++i) {
-    const j = i * 4;
-    const k = i * 3;
+//   for (let i = 0; i < height * width; ++i) {
+//     const j = i * 4;
+//     const k = i * 3;
 
-    imageData.data[j + 0] = data[k + 0];
-    imageData.data[j + 1] = data[k + 1];
-    imageData.data[j + 2] = data[k + 2];
-    imageData.data[j + 3] = 255;
-  }
+//     imageData.data[j + 0] = data[k + 0];
+//     imageData.data[j + 1] = data[k + 1];
+//     imageData.data[j + 2] = data[k + 2];
+//     imageData.data[j + 3] = 255;
+//   }
 
-  ctx.putImageData(imageData, 0, 0);
-}
+//   ctx.putImageData(imageData, 0, 0);
+// }
 
-/**
- * Draw an image on a canvas
- */
-export function renderImageToCanvas(image, size, canvas) {
-  canvas.width = size[0];
-  canvas.height = size[1];
-  const ctx = canvas.getContext('2d');
+// /**
+//  * Draw an image on a canvas
+//  */
+// export function renderImageToCanvas(image, size, canvas) {
+//   canvas.width = size[0];
+//   canvas.height = size[1];
+//   const ctx = canvas.getContext('2d');
 
-  ctx.drawImage(image, 0, 0);
-}
+//   ctx.drawImage(image, 0, 0);
+// }
 
-/**
- * Draw heatmap values, one of the model outputs, on to the canvas
- * Read our blog post for a description of PoseNet's heatmap outputs
- * https://medium.com/tensorflow/real-time-human-pose-estimation-in-the-browser-with-tensorflow-js-7dd0bc881cd5
- */
-export function drawHeatMapValues(heatMapValues, outputStride, canvas) {
-  const ctx = canvas.getContext('2d');
-  const radius = 5;
-  const scaledValues = heatMapValues.mul(tf.scalar(outputStride, 'int32'));
+// /**
+//  * Draw heatmap values, one of the model outputs, on to the canvas
+//  * Read our blog post for a description of PoseNet's heatmap outputs
+//  * https://medium.com/tensorflow/real-time-human-pose-estimation-in-the-browser-with-tensorflow-js-7dd0bc881cd5
+//  */
+// export function drawHeatMapValues(heatMapValues, outputStride, canvas) {
+//   const ctx = canvas.getContext('2d');
+//   const radius = 5;
+//   const scaledValues = heatMapValues.mul(tf.scalar(outputStride, 'int32'));
 
-  drawPoints(ctx, scaledValues, radius, color);
-}
+//   drawPoints(ctx, scaledValues, radius, color);
+// }
 
-/**
- * Used by the drawHeatMapValues method to draw heatmap points on to
- * the canvas
- */
-function drawPoints(ctx, points, radius, color) {
-  const data = points.buffer().values;
+// /**
+//  * Used by the drawHeatMapValues method to draw heatmap points on to
+//  * the canvas
+//  */
+// function drawPoints(ctx, points, radius, color) {
+//   const data = points.buffer().values;
 
-  for (let i = 0; i < data.length; i += 2) {
-    const pointY = data[i];
-    const pointX = data[i + 1];
+//   for (let i = 0; i < data.length; i += 2) {
+//     const pointY = data[i];
+//     const pointX = data[i + 1];
 
-    if (pointX !== 0 && pointY !== 0) {
-      ctx.beginPath();
-      ctx.arc(pointX, pointY, radius, 0, 2 * Math.PI);
-      ctx.fillStyle = color;
-      ctx.fill();
-    }
-  }
-}
+//     if (pointX !== 0 && pointY !== 0) {
+//       ctx.beginPath();
+//       ctx.arc(pointX, pointY, radius, 0, 2 * Math.PI);
+//       ctx.fillStyle = color;
+//       ctx.fill();
+//     }
+//   }
+// }
 
-function isAndroid() {
-  return /Android/i.test(navigator.userAgent);
-}
+// function isAndroid() {
+//   return /Android/i.test(navigator.userAgent);
+// }
 
-function isiOS() {
-  return /iPhone|iPad|iPod/i.test(navigator.userAgent);
-}
+// function isiOS() {
+//   return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+// }
 
-export function isMobile() {
-  return isAndroid() || isiOS();
-}
+// export function isMobile() {
+//   return isAndroid() || isiOS();
+// }
 
-function setDatGuiPropertyCss(propertyText, liCssString, spanCssString = '') {
-  var spans = document.getElementsByClassName('property-name');
-  for (var i = 0; i < spans.length; i++) {
-    var text = spans[i].textContent || spans[i].innerText;
-    if (text == propertyText) {
-      spans[i].parentNode.parentNode.style = liCssString;
-      if (spanCssString !== '') {
-        spans[i].style = spanCssString;
-      }
-    }
-  }
-}
+// function setDatGuiPropertyCss(propertyText, liCssString, spanCssString = '') {
+//   var spans = document.getElementsByClassName('property-name');
+//   for (var i = 0; i < spans.length; i++) {
+//     var text = spans[i].textContent || spans[i].innerText;
+//     if (text == propertyText) {
+//       spans[i].parentNode.parentNode.style = liCssString;
+//       if (spanCssString !== '') {
+//         spans[i].style = spanCssString;
+//       }
+//     }
+//   }
+// }
 
-export function updateTryResNetButtonDatGuiCss() {
-  setDatGuiPropertyCss(
-      tryResNetButtonText, tryResNetButtonBackgroundCss,
-      tryResNetButtonTextCss);
-}
+// export function updateTryResNetButtonDatGuiCss() {
+//   setDatGuiPropertyCss(
+//       tryResNetButtonText, tryResNetButtonBackgroundCss,
+//       tryResNetButtonTextCss);
+// }
 
-/**
- * Toggles between the loading UI and the main canvas UI.
- */
-export function toggleLoadingUI(
-  showLoadingUI, loadingDivId = 'loading', mainDivId = 'main') {
-  if (showLoadingUI) {
-    document.getElementById(loadingDivId).style.display = 'block';
-    document.getElementById(mainDivId).style.display = 'none';
-  } else {
-    document.getElementById(loadingDivId).style.display = 'none';
-    document.getElementById(mainDivId).style.display = 'block';
-  }
-}
+// /**
+//  * Toggles between the loading UI and the main canvas UI.
+//  */
+// export function toggleLoadingUI(
+//   showLoadingUI, loadingDivId = 'loading', mainDivId = 'main') {
+//   if (showLoadingUI) {
+//     document.getElementById(loadingDivId).style.display = 'block';
+//     document.getElementById(mainDivId).style.display = 'none';
+//   } else {
+//     document.getElementById(loadingDivId).style.display = 'none';
+//     document.getElementById(mainDivId).style.display = 'block';
+//   }
+// }
 
-function toTuple({y, x}) {
-  return [y, x];
-}
+// function toTuple({y, x}) {
+//   return [y, x];
+// }
 
