14d66cc8c9b498691434cc81ac452a84faa449ed
Dani <dreamboy.dani@gmail.com>
2020-11-03 11:41:15 -0600

    Persist of user data upon refresh

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,27 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.scss';
 import Home from "./routes/Home";
 import Game from "./routes/Game";
-import axios from 'axios';
+
+/**
+ * Cutom Hook
+ * @param localStorage key to get and set the data in the localStorage
+ */
+const useStateWithLocalStorage = localStorageKey => {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("userInLocalStorge"))
+  )
+
+  // Set data in storage
+  useEffect(() => {
+    localStorage.setItem("userInLocalStorge", JSON.stringify(user))
+  }, [user]);
+
+  return [user, setUser]
+}
 
 function App() {
 
-  const [user, setUser] = useState({
-    isLoggedIn: false,
-    user: {}
-  })
-
-  // useEffect(() => {
-  //   axios.get(process.env.REACT_APP_IPSTACK_API_URL)
-  //     .then(res => console.log(res, "check"))
-  // }, [])
+  const [user, setUser] = useStateWithLocalStorage("userInLocalStorge")
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
