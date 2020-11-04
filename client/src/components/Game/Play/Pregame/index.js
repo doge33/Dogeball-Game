a1502a0582c07b6a5c0f143387a9664b25e35f6e
doge33 <sandylx33@gmail.com>
2020-11-04 19:30:37 +0000

    added navbar to pregame

--- client/src/components/Game/Play/Pregame/index.js
+++ client/src/components/Game/Play/Pregame/index.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState, useContext} from "react";
+import {useHistory} from "react-router-dom";
 import Navbar from "../../../Navbar";
 import Button from "../../../Button";
 import Tutorial from "../../Tutorial";
@@ -7,33 +8,38 @@ import tutorialContext from "../../../../Context/tutorialContext";
 import Leaderboard from "../../Lobby/Leaderboard";
 import UserHistory from "../../Lobby/UserHistory";
 import "../../Tutorial.scss"; //=> conflict with App.scss
-
+import axios from 'axios';
 
 function Pregame(props) {
 
   //let tutorialOpen = false;
   const [tutorialActive, setTutorialActive] = useState(false)
 
-  const {user} = useContext(userContext)
-  function expandTutorial(){
-    setTutorialActive(!tutorialActive);
-    //tutorialOpen = true;
-    //return <Tutorial />
+  const {user, setUser} = useContext(userContext)
+  
+  const history = useHistory()
+  const handleLogout = () => {
+    axios.post("/logout", {}, { withCredentials: true })
+      .then((res) => {
+        setUser({
+          ...user,
+          isLoggedIn: false,
+          user: {}
+        })
+        history.push('/')
+      })
   }
+  
+    function expandTutorial(){
+        setTutorialActive(!tutorialActive);
 
-
-  //   if(tutorialOpen){
-  //   return <Tutorial />
-  // }
-
-
-
+      }
   //insert tutorial here
   return (
 
     <tutorialContext.Provider value={{tutorialActive, setTutorialActive}}>
     <div className="animate-in pre-game">
-    <Navbar  user={user}/> 
+    <Navbar  user={user.user} logout={handleLogout} /> 
       <div class="lines">
         <div class="line"></div>
         <div class="line"></div>
